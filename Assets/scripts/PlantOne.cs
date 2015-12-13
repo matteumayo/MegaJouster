db2d3bb231709c29a15862b301965386827f1aac
matteumayo <matteumayo@gmail.com>
2015-12-12 21:09:08 -0800

    ya

---
+++ Assets/scripts/PlantOne.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlantOne : MonoBehaviour {
+
+	public GameObject PlantOneObj;
+	public GameObject PlayerObj;
+	public bool StandingOnFlower = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D other) {
+		print ("Yes");
+		StandingOnFlower = true;
+
+		}
+	void OnTriggerExit2D(Collider2D other){
+		StandingOnFlower = false;
+	}
+
+	void Update(){
+		if (StandingOnFlower == true) {
+			if (Input.GetKeyDown (KeyCode.LeftControl) == true) {
+				print ("o ya");
+
+
+			
+			}
+		}
+	}
+}
